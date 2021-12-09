c9a3c641d302e2b0a0357748a0edf93cb7a8e550
naeunchan <eunchan0492@naver.com>
2021-12-09 09:50:17 +0900

    modal complete

--- client/src/components/My/index.jsx
+++ client/src/components/My/index.jsx
@@ -19,270 +19,272 @@ import Select from "@material-ui/core/Select";
 import { TEAMCODE } from "@api";
 
 const Title = styled.div`
-    display: flex;
-    font-size: 35px;
-    font-weight: bold;
-    margin: 30px 0 20px 0;
+	display: flex;
+	font-size: 35px;
+	font-weight: bold;
+	margin: 30px 0 20px 0;
 `;
 
 const ButtonContainer = styled.div`
-    display: flex;
-    justify-content: space-around;
-    width: 50%;
-    margin-top: 50px;
+	display: flex;
+	justify-content: space-around;
+	width: 50%;
+	margin-top: 50px;
 
-    @media ${styles.media.sm} {
-        width: 100%;
-        flex-direction: column;
-        align-items: center;
-        font-size: ${styles.fontStyle.small};
+	@media ${styles.media.sm} {
+		width: 100%;
+		flex-direction: column;
+		align-items: center;
+		font-size: ${styles.fontStyle.small};
 
-        Button:nth-of-type(1) {
-            margin-bottom: 10px;
-        }
-    }
+		Button:nth-of-type(1) {
+			margin-bottom: 10px;
+		}
+	}
 `;
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        display: "flex",
-        flexWrap: "wrap",
-        flexDirection: "column",
-    },
-    margin: {
-        margin: "10px 0",
-    },
-    withoutLabel: {
-        marginTop: theme.spacing(3),
-    },
-    textField: {
-        width: "25ch",
-    },
+	root: {
+		display: "flex",
+		flexWrap: "wrap",
+		flexDirection: "column",
+	},
+	margin: {
+		margin: "10px 0",
+	},
+	withoutLabel: {
+		marginTop: theme.spacing(3),
+	},
+	textField: {
+		width: "25ch",
+	},
 }));
 
 const My = ({ onClick, ...props }) => {
-    const classes = useStyles();
-    const [userInfo, setUserInfo] = useState(null);
-    const PID = parseInt(sessionStorage.getItem("PID"));
-    const [values, setValues] = useState({
-        password: "",
-        team: "",
-        phone: "",
-        nickname: "",
-        showPassword: false,
-        showPassword2: false,
-    });
+	const classes = useStyles();
+	const [userInfo, setUserInfo] = useState(null);
+	const PID = parseInt(sessionStorage.getItem("PID"));
+	const [showPW, setShowPW] = useState(false);
 
-    const handleMouseDownPassword = (event) => {
-        event.preventDefault();
-    };
+	const handleMouseDownPassword = (event) => {
+		event.preventDefault();
+	};
 
-    const handleClickShowPassword = () => {
-        setValues({ ...values, showPassword: !values.showPassword });
-    };
+	const handleClickShowPassword = () => {
+		setShowPW(!showPW.showPassword);
+	};
 
-    const handleChangePW = (event) => {
-        setValues({ ...values, password: event.target.value });
-    };
+	const handleChangePW = (event) => {
+		setUserInfo({ ...userInfo, password: event.target.value });
+	};
 
-    const handleChangeTeam = (event) => {
-        setValues({ ...values, team: event.target.value });
-    };
+	const handleChangeTeam = (event) => {
+		setUserInfo({ ...userInfo, team: event.target.value });
+	};
 
-    const handleChangePhone = (event) => {
-        setValues({ ...values, phone: event.target.value });
-    };
+	const handleChangePhone = (event) => {
+		setUserInfo({ ...userInfo, phone: event.target.value });
+	};
 
-    const handleChangeNickname = (event) => {
-        setValues({ ...values, nickname: event.target.value });
-    };
+	const handleChangeNickname = (event) => {
+		setUserInfo({ ...userInfo, nickname: event.target.value });
+	};
 
-    const handleClickEditButton = () => {
-        const { password, team, phone, nickname } = values;
-        let errorMsg = "";
-        let isError = false;
+	const handleClickEditButton = () => {
+		const { password, team, phone, nickname } = userInfo;
+		let errorMsg = "";
+		let isError = false;
 
-        if (password.length < 8 || password.length > 20) {
-            errorMsg = "비밀번호는 8자 이상, 20자 이하로 입력해주세요!";
-            isError = true;
-        } else if (team === "") {
-            errorMsg = "소속을 선택해주세요!";
-            isError = true;
-        } else if (phone === "") {
-            errorMsg = "휴대폰 번호를 입력해주세요!";
-            isError = true;
-        }
+		if (password.length < 8 || password.length > 20) {
+			errorMsg = "비밀번호는 8자 이상, 20자 이하로 입력해주세요!";
+			isError = true;
+		} else if (team === "") {
+			errorMsg = "소속을 선택해주세요!";
+			isError = true;
+		} else if (phone === "") {
+			errorMsg = "휴대폰 번호를 입력해주세요!";
+			isError = true;
+		}
 
-        if (isError) {
-            Swal.fire({
-                icon: "warning",
-                title: errorMsg,
-            });
-        } else {
-            axios
-                .post("/api/my", null, {
-                    params: {
-                        PID,
-                        password,
-                        team,
-                        phone,
-                        nickname,
-                    },
-                })
-                .then((res) => {
-                    if (res.data === "success") {
-                        Swal.fire({
-                            icon: "success",
-                            title: "정보가 변경되었습니다!",
-                        });
-                    } else {
-                        Swal.fire({
-                            icon: "warning",
-                            title: "잠시 후 다시 시도해주세요!",
-                        });
-                    }
-                })
-                .catch((err) => {
-                    Swal.fire({
-                        icon: "warning",
-                        title: "에러가 발생했습니다!",
-                    });
-                });
-        }
-    };
+		if (isError) {
+			Swal.fire({
+				icon: "warning",
+				title: errorMsg,
+			});
+		} else {
+			axios
+				.post("/api/my", null, {
+					params: {
+						PID,
+						password,
+						team,
+						phone,
+						nickname,
+					},
+				})
+				.then((res) => {
+					if (res.data === "success") {
+						Swal.fire({
+							icon: "success",
+							title: "정보가 변경되었습니다!",
+						}).then(() => {
+							onClick();
+						});
+					} else {
+						Swal.fire({
+							icon: "warning",
+							title: "잠시 후 다시 시도해주세요!",
+						});
+					}
+				})
+				.catch((err) => {
+					Swal.fire({
+						icon: "warning",
+						title: "에러가 발생했습니다!",
+					});
+				});
+		}
+	};
 
-    useEffect(() => {
-        axios
-            .get("/api/my", {
-                params: { PID },
-            })
-            .then((res) => {
-                if (res.data.PID === PID) {
-                    setUserInfo(res.data);
-                } else {
-                    Swal.fire({
-                        icon: "error",
-                        title: res.data,
-                    });
-                }
-            })
-            .catch((err) => {
-                Swal.fire({
-                    icon: "error",
-                    title: "에러가 발생했습니다!",
-                });
-            });
-    }, [PID]);
+	useEffect(() => {
+		axios
+			.get("/api/my", {
+				params: { PID },
+			})
+			.then((res) => {
+				if (res.data.PID === PID) {
+					setUserInfo(res.data);
+				} else {
+					Swal.fire({
+						icon: "error",
+						title: res.data,
+					});
+				}
+			})
+			.catch((err) => {
+				Swal.fire({
+					icon: "error",
+					title: "에러가 발생했습니다!",
+				});
+			});
+	}, [PID]);
 
-    return (
-        <Modal>
-            <Title>마이 페이지</Title>
-            {userInfo && (
-                <>
-                    <div className={classes.root}>
-                        <TextField
-                            className={classes.margin}
-                            id="standard-read-only-input"
-                            label="사번"
-                            defaultValue={userInfo.PID}
-                            InputProps={{
-                                readOnly: true,
-                            }}
-                        />
-                        <FormControl className={clsx(classes.margin, classes.textField)} required>
-                            <InputLabel htmlFor="standard-adornment-password">비밀번호</InputLabel>
-                            <Input
-                                id="standard-adornment-password"
-                                type={values.showPassword ? "text" : "password"}
-                                endAdornment={
-                                    <InputAdornment position="end">
-                                        <IconButton
-                                            aria-label="toggle password visibility"
-                                            onClick={handleClickShowPassword}
-                                            onMouseDown={handleMouseDownPassword}
-                                        >
-                                            {values.showPassword ? (
-                                                <Visibility />
-                                            ) : (
-                                                <VisibilityOff />
-                                            )}
-                                        </IconButton>
-                                    </InputAdornment>
-                                }
-                                onChange={handleChangePW}
-                                defaultValue={userInfo.password}
-                            />
-                        </FormControl>
-                        <TextField
-                            className={classes.margin}
-                            id="standard-read-only-input"
-                            label="이름"
-                            defaultValue={userInfo.name}
-                            InputProps={{
-                                readOnly: true,
-                            }}
-                        />
-                        <TextField
-                            className={classes.margin}
-                            id="standard-read-only-input"
-                            label="이메일"
-                            defaultValue={userInfo.email}
-                            InputProps={{
-                                readOnly: true,
-                            }}
-                        />
-                        <FormControl className={classes.margin} required>
-                            <InputLabel id="demo-simple-select-label">소속</InputLabel>
-                            <Select
-                                labelId="demo-simple-select-label"
-                                id="demo-simple-select"
-                                value={userInfo.team}
-                                onChange={handleChangeTeam}
-                            >
-                                <MenuItem value={1}>페이북개발팀</MenuItem>
-                                <MenuItem value={2}>페이북회원팀</MenuItem>
-                                <MenuItem value={3}>페이북결제팀</MenuItem>
-                                <MenuItem value={4}>페이북채널팀</MenuItem>
-                                <MenuItem value={5}>마이데이터개발팀</MenuItem>
-                                <MenuItem value={6}>CB사업팀</MenuItem>
-                            </Select>
-                        </FormControl>
-                        <TextField
-                            className={classes.margin}
-                            required
-                            id="standard-required"
-                            label="핸드폰"
-                            defaultValue={userInfo.phone}
-                            onChange={handleChangePhone}
-                        />
-                        <TextField
-                            className={classes.margin}
-                            id="standard"
-                            label="닉네임"
-                            defaultValue={userInfo.nickname}
-                            onChange={handleChangeNickname}
-                        />
-                    </div>
-                    <ButtonContainer>
-                        <Button
-                            backgroundColor={styles.color.confirm}
-                            style={{ width: "100px" }}
-                            onClick={handleClickEditButton}
-                        >
-                            수정하기
-                        </Button>
-                        <Button
-                            backgroundColor={styles.color.cancel}
-                            style={{ width: "100px" }}
-                            onClick={onClick}
-                        >
-                            돌아가기
-                        </Button>
-                    </ButtonContainer>
-                </>
-            )}
-        </Modal>
-    );
+	return (
+		<Modal>
+			<Title>마이 페이지</Title>
+			{userInfo && (
+				<>
+					<div className={classes.root}>
+						<TextField
+							className={classes.margin}
+							id="standard-read-only-input"
+							label="사번"
+							defaultValue={userInfo.PID}
+							InputProps={{
+								readOnly: true,
+							}}
+							disabled
+						/>
+						<FormControl
+							className={clsx(classes.margin, classes.textField)}
+							required>
+							<InputLabel htmlFor="standard-adornment-password">
+								비밀번호
+							</InputLabel>
+							<Input
+								id="standard-adornment-password"
+								type={showPW.showPassword ? "text" : "password"}
+								endAdornment={
+									<InputAdornment position="end">
+										<IconButton
+											aria-label="toggle password visibility"
+											onClick={handleClickShowPassword}
+											onMouseDown={
+												handleMouseDownPassword
+											}>
+											{showPW.showPassword ? (
+												<Visibility />
+											) : (
+												<VisibilityOff />
+											)}
+										</IconButton>
+									</InputAdornment>
+								}
+								onChange={handleChangePW}
+								defaultValue={userInfo.password}
+							/>
+						</FormControl>
+						<TextField
+							className={classes.margin}
+							id="standard-read-only-input"
+							label="이름"
+							defaultValue={userInfo.name}
+							InputProps={{
+								readOnly: true,
+							}}
+							disabled
+						/>
+						<TextField
+							className={classes.margin}
+							id="standard-read-only-input"
+							label="이메일"
+							defaultValue={userInfo.email}
+							InputProps={{
+								readOnly: true,
+							}}
+							disabled
+						/>
+						<FormControl className={classes.margin} required>
+							<InputLabel id="demo-simple-select-label">
+								소속
+							</InputLabel>
+							<Select
+								labelId="demo-simple-select-label"
+								id="demo-simple-select"
+								value={userInfo.team}
+								onChange={handleChangeTeam}>
+								<MenuItem value={1}>페이북개발팀</MenuItem>
+								<MenuItem value={2}>페이북회원팀</MenuItem>
+								<MenuItem value={3}>페이북결제팀</MenuItem>
+								<MenuItem value={4}>페이북채널팀</MenuItem>
+								<MenuItem value={5}>마이데이터개발팀</MenuItem>
+								<MenuItem value={6}>CB사업팀</MenuItem>
+							</Select>
+						</FormControl>
+						<TextField
+							className={classes.margin}
+							required
+							id="standard-required"
+							label="핸드폰"
+							defaultValue={userInfo.phone}
+							onChange={handleChangePhone}
+						/>
+						<TextField
+							className={classes.margin}
+							id="standard"
+							label="닉네임"
+							defaultValue={userInfo.nickname}
+							onChange={handleChangeNickname}
+						/>
+					</div>
+					<ButtonContainer>
+						<Button
+							backgroundColor={styles.color.confirm}
+							style={{ width: "100px" }}
+							onClick={handleClickEditButton}>
+							수정하기
+						</Button>
+						<Button
+							backgroundColor={styles.color.cancel}
+							style={{ width: "100px" }}
+							onClick={onClick}>
+							돌아가기
+						</Button>
+					</ButtonContainer>
+				</>
+			)}
+		</Modal>
+	);
 };
 
 export default My;
--- server/routes/api.js
+++ server/routes/api.js
@@ -5,101 +5,109 @@ const router = express.Router();
 const checkPIDSql = "select count(*) as result from user where PID = ?";
 
 router.get("/login", (req, res) => {
-    res.send({ data: "qwer" });
+	res.send({ data: "qwer" });
 });
 
 router.post("/login", (req, res) => {
-    const { PID, password } = req.query;
+	const { PID, password } = req.query;
 
-    db.query(checkPIDSql, PID, (err, data) => {
-        if (!err) {
-            if (data[0].result) {
-                const checkPWSql = "select * from user where PID = ? and password = ?";
+	db.query(checkPIDSql, PID, (err, data) => {
+		if (!err) {
+			if (data[0].result) {
+				const checkPWSql =
+					"select * from user where PID = ? and password = ?";
 
-                db.query(checkPWSql, [PID, password], (err, data) => {
-                    if (!err) {
-                        if (!data[0]) {
-                            res.send("비밀번호가 일치하지 않습니다!");
-                        } else {
-                            res.send(data[0]);
-                        }
-                    } else {
-                        res.send("에러가 발생했습니다!");
-                    }
-                });
-            } else {
-                res.send("등록되지 않은 회원입니다!");
-            }
-        } else {
-            res.send("에러가 발생했습니다!");
-        }
-    });
+				db.query(checkPWSql, [PID, password], (err, data) => {
+					if (!err) {
+						if (!data[0]) {
+							res.send("비밀번호가 일치하지 않습니다!");
+						} else {
+							res.send(data[0]);
+						}
+					} else {
+						res.send("에러가 발생했습니다!");
+					}
+				});
+			} else {
+				res.send("등록되지 않은 회원입니다!");
+			}
+		} else {
+			res.send("에러가 발생했습니다!");
+		}
+	});
 });
 
 router.post("/signup", (req, res) => {
-    const { PID, password, name, email, team, phone, nickname } = req.query;
+	const { PID, password, name, email, team, phone, nickname } = req.query;
 
-    const signUpSql =
-        "insert into user (PID, email, password, name, team, phone, nickname) values(?, ?, ?, ?, ?, ?, ?)";
+	const signUpSql =
+		"insert into user (PID, email, password, name, team, phone, nickname) values(?, ?, ?, ?, ?, ?, ?)";
 
-    const user = [
-        parseInt(PID),
-        email,
-        password,
-        name,
-        team,
-        phone,
-        nickname === "" ? null : nickname,
-    ];
+	const user = [
+		parseInt(PID),
+		email,
+		password,
+		name,
+		parseInt(team),
+		phone,
+		nickname === "" ? null : nickname,
+	];
 
-    // PID 중복 체크
-    db.query(checkPIDSql, PID, (err, data) => {
-        if (!err) {
-            if (data[0].result) {
-                res.send("duplicated");
-            } else {
-                db.query(signUpSql, user, (err, data) => {
-                    if (!err) {
-                        res.send("success");
-                    } else {
-                        res.send("회원가입에 실패했습니다!\n다시 시도해주세요!");
-                    }
-                });
-            }
-        }
-    });
+	// PID 중복 체크
+	db.query(checkPIDSql, PID, (err, data) => {
+		if (!err) {
+			if (data[0].result) {
+				res.send("duplicated");
+			} else {
+				db.query(signUpSql, user, (err, data) => {
+					if (!err) {
+						res.send("success");
+					} else {
+						res.send(
+							"회원가입에 실패했습니다!\n다시 시도해주세요!",
+						);
+					}
+				});
+			}
+		}
+	});
 });
 
 router.get("/my", (req, res) => {
-    const PID = parseInt(req.query.PID);
+	const PID = parseInt(req.query.PID);
 
-    const getUserInfoSql = "select * from user where PID = ?";
+	const getUserInfoSql = "select * from user where PID = ?";
 
-    db.query(getUserInfoSql, PID, (err, data) => {
-        if (!err) {
-            const { PID, email, password, name, team, phone, nickname } = data[0];
+	db.query(getUserInfoSql, PID, (err, data) => {
+		if (!err) {
+			const { PID, email, password, name, team, phone, nickname } =
+				data[0];
 
-            res.send({ PID, email, password, name, team, phone, nickname });
-        } else {
-            console.log(err);
-            res.send("데이터를 가져오지 못햇습니다!");
-        }
-    });
+			res.send({ PID, email, password, name, team, phone, nickname });
+		} else {
+			console.log(err);
+			res.send("데이터를 가져오지 못햇습니다!");
+		}
+	});
 });
 
 router.post("/my", (req, res) => {
-    const { PID, password, team, phone, nickname } = req.query;
+	const { PID, password, team, phone, nickname } = req.query;
 
-    const updateUserInfoSql =
-        "update user set password = ?, team = ?, phone = ?, nickname = ? where PID = ?";
+	const updateUserInfoSql =
+		"update user set password = ?, team = ?, phone = ?, nickname = ? where PID = ?";
 
-    db.query(updateUserInfoSql, [password, team, phone, nickname, PID], (err, data) => {
-        if (!err) {
-            res.send("success");
-        } else {
-            res.send("fail");
-        }
-    });
+	db.query(
+		updateUserInfoSql,
+		[password, parseInt(team), phone, nickname, parseInt(PID)],
+		(err, data) => {
+			if (!err) {
+				res.send("success");
+			} else {
+				res.send("fail");
+			}
+		},
+	);
 });
 
 module.exports = router;
